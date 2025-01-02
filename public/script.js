826a05514e7a6fa506706e7a86220ebdad34c51d
ram15 <ram153486@gmail.com>
2025-01-02 22:21:39 +0200

    Both maps work. need to change gove map

--- public/script.js
+++ public/script.js
@@ -10,11 +10,14 @@ const startConversion = async () => {
   // Display loading spinner
   const loadingDiv = document.getElementById("loading");
   const outputDiv = document.getElementById("output");
-  const mapPreviewDiv = document.getElementById("mapPreview");
-  const mapFrame = document.getElementById("mapFrame");
+  const govMapPreviewDiv = document.getElementById("mapPreview");
+  const googleMapPreviewDiv = document.getElementById("googleMapPreview");
+  const govMapFrame = document.getElementById("govMapFrame");
+  const googleMapFrame = document.getElementById("googleMapFrame");
   loadingDiv.style.display = "block";
   outputDiv.innerHTML = "";
-  mapPreviewDiv.style.display = "none";
+  govMapPreviewDiv.style.display = "none";
+  googleMapPreviewDiv.style.display = "none";
 
   try {
     const response = await fetch("/convert", {
@@ -35,10 +38,20 @@ const startConversion = async () => {
         <a href="${data.googleMapsUrl}" target="_blank">${data.googleMapsUrl}</a>
       `;
 
-      // Update map preview
-      const mapUrl = `https://www.govmap.gov.il/map.html?lay=Matara_MItham,Matara_Mig&bs=Matara_MItham|ACTIVEPROJECTID~${projectInput}`;
-      mapFrame.src = mapUrl;
-      mapPreviewDiv.style.display = "block";
+      // Update gov map preview
+      const govMapUrl = `https://www.govmap.gov.il/map.html?lay=Matara_MItham,Matara_Mig&bs=Matara_MItham|ACTIVEPROJECTID~${projectInput}`;
+      govMapFrame.src = govMapUrl;
+      govMapPreviewDiv.style.display = "block";
+      
+      // Update Google Maps preview
+      const googleMapsMatch = data.googleMapsUrl.match(/place\/(-?\d+\.\d+),(-?\d+\.\d+)/);
+      if (googleMapsMatch) {
+        const lat = googleMapsMatch[1];
+        const lng = googleMapsMatch[2];
+        const googleMapsEmbedUrl = `https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d4063.3509927002774!2d${lng}!3d${lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzHCsDQwJzA5LjQiTiAzNMKwMzUnNDguMCJF!5e1!3m2!1sen!2sil`;
+        googleMapFrame.src = googleMapsEmbedUrl;
+        googleMapPreviewDiv.style.display = "block";
+      }
     } else {
       outputDiv.innerHTML = `<p>Error: ${data.error}</p>`;
     }
