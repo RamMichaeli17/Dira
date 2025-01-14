19f3d1dea720ff466b235a854024d4d35560d8b2
ram15 <ram153486@gmail.com>
2025-01-14 19:23:19 +0200

    Fixed coordinates

--- config/coordinates-config.js
+++ config/coordinates-config.js
@@ -4,7 +4,8 @@
  * ITM (Israeli Transverse Mercator) projection definition
  * Used for converting Israeli grid coordinates to WGS84
  */
-const ITM = "+proj=tmerc +lat_0=31.73439361111111 +lon_0=35.20451694444444 +k=1.0000067 +x_0=219529.584 +y_0=626907.39 +ellps=GRS80 +towgs84=0,0,-48,0,0,0,0 +units=m +no_defs";
+const ITM =
+  "+proj=tmerc +lat_0=31.7343936111111 +lon_0=35.2045169444444 +k=1.0000067 +x_0=219529.584 +y_0=626907.39 +ellps=GRS80 +towgs84=23.772,17.49,17.859,-0.3132,-1.85274,1.67299,-5.4262 +units=m +no_defs +type=crs";
 
 /**
  * WGS84 projection definition
